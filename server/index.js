b42402bc5f404917a25b009848beacb30288fe2d
jitendra lodhi <jitendralodhidev@gmail.com>
2024-07-03 11:47:50 +0530

    Backend code optimization and added doc strings

--- server/index.js
+++ server/index.js
@@ -1,16 +1,21 @@
+// Importing Express module and other packages
 const express = require("express");
 const { ApolloServer } = require("@apollo/server");
 const { expressMiddleware } = require("@apollo/server/express4");
 const bodyParser = require("body-parser");
+const app = express();
 const cors = require("cors");
 
-// Importing data
+app.use(bodyParser.json());
+app.use(cors());
+
+// Importing data files
 const { USERS } = require("./user");
 const { TODOS } = require("./todo");
 
 async function startServer() {
-  const app = express();
-  const server = new ApolloServer({
+
+  const server = new ApolloServer({ // Creating an ApolloServer instance with type definitions and resolvers
     typeDefs: `
         type User {
             id: ID!
@@ -29,17 +34,18 @@ async function startServer() {
         }
 
         type Query {
-            getTodos: [Todo] 
+            getTodos: [Todo]
             getAllUsers: [User]
             getUser(id: ID!): User
         }
-
     `,
     resolvers: {
       Todo: {
+        // Resolver to get user for a todo item
         user: (todo) => USERS.find((e) => e.id === todo.id),
       },
       Query: {
+        // Resolvers for query operations
         getTodos: () => TODOS,
         getAllUsers: () => USERS,
         getUser: async (parent, { id }) => USERS.find((e) => e.id === id),
@@ -47,14 +53,10 @@ async function startServer() {
     },
   });
 
-  app.use(bodyParser.json());
-  app.use(cors());
+  await server.start(); // Starting Apollo Server
+  app.use("/graphql", expressMiddleware(server)); // Using Apollo Server middleware for handling GraphQL requests
 
-  await server.start();
-
-  app.use("/graphql", expressMiddleware(server));
-
-  app.listen(8000, () => console.log("Serevr Started at PORT 8000"));
+  app.listen(8000, () => console.log("Server Started at PORT 8000"));
 }
 
-startServer();
+startServer(); // Initiating the server start process
